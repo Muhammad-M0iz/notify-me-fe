ef417f403a46c84d208afe270bcc97e08b0f36ef
Muhammad Moiz <139315594+Muhammad-M0iz@users.noreply.github.com>
2025-02-20 11:25:17 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,12 @@ client.setEndpoint(config.appwriteUrl);
 const account = new Account(client);
 const { email, password } = RandomCredentials();
 function App() {
-  const slides={
-      https://i.ibb.co/ncrXc2V/1.png
-      https://i.ibb.co/B3s7v4h/2.png
-      https://i.ibb.co/XXR8kzF/3.png
-      https://i.ibb.co/yg7BSdM/4.png
-  }
+  const slides=[
+      "https://i.ibb.co/ncrXc2V/1.png",
+      "https://i.ibb.co/B3s7v4h/2.png",
+      "https://i.ibb.co/XXR8kzF/3.png",
+      "https://i.ibb.co/yg7BSdM/4.png"
+  ]
 
   const [register,setRegister]=useState(false);
   const [clicked,setClicked]=useState(false);

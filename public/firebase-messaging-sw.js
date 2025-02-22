9402d3c6f651e8ec7f4d5f0234419b5f8f9ea8da
Muhammad Moiz <139315594+Muhammad-M0iz@users.noreply.github.com>
2025-02-22 12:51:52 +0500

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,27 +4,18 @@ importScripts(
 );
 
 var firebaseConfig = {
-    apiKey: "AIzaSyDLhYWCoJqMl1STd5wxH-GLrlLDknF3Bbk",
+  apiKey: "AIzaSyDLhYWCoJqMl1STd5wxH-GLrlLDknF3Bbk",
   authDomain: "nofify-lms.firebaseapp.com",
   projectId: "nofify-lms",
   storageBucket: "nofify-lms.firebasestorage.app",
   messagingSenderId: "771809860808",
-  appId: "1:771809860808:web:29778a53249921dfc219fd"
-  };
+  appId: "1:771809860808:web:29778a53249921dfc219fd",
+};
 
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
+// Listen for background messages but do NOT show a notification
 messaging.onBackgroundMessage((payload) => {
-  console.log(
-    "[firebase-messaging-sw.js] Received background message ",
-    payload
-  );
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: payload.notification.image,
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
+  console.log("[firebase-messaging-sw.js] Received background message", payload);
 });
